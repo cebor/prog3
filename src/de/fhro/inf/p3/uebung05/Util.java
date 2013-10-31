6c1b5c635ab81726f6a38c47d8f1f603419d40db
Felix <felix@stkn.org>
2013-10-31 11:13:50 +0100

    fix

--- src/de/fhro/inf/p3/uebung05/Util.java
+++ src/de/fhro/inf/p3/uebung05/Util.java
@@ -9,42 +9,42 @@ import java.util.List;
 public class Util {
 
     public static List<Integer> range(final int size) {
-        if (size >= 0) {
-            return new AbstractList<Integer>() {
-                @Override
-                public Integer get(int i) {
-                    if (i >= 0 && i < size())
-                        return i;
-                    else
-                        throw new IndexOutOfBoundsException();
-                }
+        if (size < 0)
+            throw new IllegalArgumentException();
 
-                @Override
-                public int size() {
-                    return size;
-                }
-            };
-        } else
-            throw new IndexOutOfBoundsException();
+        return new AbstractList<Integer>() {
+            @Override
+            public Integer get(int i) {
+                if (i >= 0 && i < size())
+                    return i;
+                else
+                    throw new IndexOutOfBoundsException();
+            }
+
+            @Override
+            public int size() {
+                return size;
+            }
+        };
     }
 
     public static List slice(final List xs, final int start, final int stop) {
-        if (stop < xs.size() && start <= stop && start >= 0 && stop >= 0)
-            return new AbstractList() {
-                @Override
-                public Object get(int i) {
-                    if (i >= 0 && i <= size())
-                        return xs.get(start + i);
-                    else
-                        throw new IndexOutOfBoundsException();
-                }
+        if (start < 0 || stop >= xs.size() || start > stop)
+            throw new IllegalArgumentException();
 
-                @Override
-                public int size() {
-                    return stop - start + 1;
-                }
-            };
-        else
-            throw new IndexOutOfBoundsException();
+        return new AbstractList() {
+            @Override
+            public Object get(int i) {
+                if (i >= 0 && i <= size())
+                    return xs.get(start + i);
+                else
+                    throw new IndexOutOfBoundsException();
+            }
+
+            @Override
+            public int size() {
+                return stop - start + 1;
+            }
+        };
     }
 }

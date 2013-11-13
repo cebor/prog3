07a11f373f7c22c2bc16d5c40995d46e57389152
Felix <felix@stkn.org>
2013-11-13 12:33:26 +0100

    fix

--- src/de/fhro/inf/p3/uebung07/ShapeStrategieRandomDirection.java
+++ src/de/fhro/inf/p3/uebung07/ShapeStrategieRandomDirection.java
@@ -8,6 +8,11 @@ import java.util.List;
 public class ShapeStrategieRandomDirection implements ShapeStrategie {
     @Override
     public void doAction(List<MovingShape> shapes, int maxX, int maxY) {
-
+        for (MovingShape mShape : shapes) {
+            mShape.move(maxX, maxY);
+            if (Zufall.randomInt(1, 10) == 1) {
+                mShape.setSpeed(Zufall.speed(20), Zufall.speed(20));
+            }
+        }
     }
 }
